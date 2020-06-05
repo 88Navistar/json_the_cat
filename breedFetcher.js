d165cb1599f3a5be476bd96d751c133be77c6638
Rob Wallator <rob@rawimpressions.ca>
2020-06-05 00:30:25 +0000

    jsonthecat filed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,24 @@
-const request = require('request');
-// let argue = process.argv.slice(2);
-// argue = [0];
+const request = require("request");
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  if (error) {
-    console.log('There was an error');
-  }
-  const data = JSON.parse(body);
-  //console.log(data);
-  //console.log(typeof data);
-  console.log(data[0].description);
+const fetchBreedDescription = function(breed, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  let breedName = breed;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback("Sorry no request details: " + error, null);
+    }
 
+    const data = JSON.parse(body);
 
-});
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(`Did not find breed ${breedName}`, null);
+    }
+  });
+};
 
-
+module.exports = { fetchBreedDescription };
 
 
