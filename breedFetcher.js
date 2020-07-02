443d2018cab5e3a92be88b06a5a1ff612a6bbd83
Rob Wallator <rob@rawimpressions.ca>
2020-07-02 19:59:28 +0000

    updated and linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,9 +2,9 @@ const request = require("request");
 
 const fetchBreedDescription = function(breed, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-  let breedName = breed;
+  breedName = breed;
   request(url, (error, response, body) => {
-    if (error) {
+    if (error || response >= 400) {
       callback("Sorry no request details: " + error, null);
     }
 
